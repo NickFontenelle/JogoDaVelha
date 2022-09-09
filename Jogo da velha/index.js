a7cb8cab9638461cb89250831dba28ce120e8a57
NickFontenelle <thenickfontenelle@gmail.com>
2022-09-09 14:12:33 -0300

    push

--- Jogo da velha/index.js
+++ Jogo da velha/index.js
@@ -4,7 +4,12 @@ let jogador = "X";
 let casa01 = document.getElementById("casa01");
 let casa02 = document.getElementById("casa02");
 let casa03 = document.getElementById("casa03");
-
+let casa04 = document.getElementById("casa04");
+let casa05 = document.getElementById("casa05");
+let casa06 = document.getElementById("casa06");
+let casa07 = document.getElementById("casa07");
+let casa08 = document.getElementById("casa08");
+let casa09 = document.getElementById("casa09");
 
 function jogada(casa) {
     if (casa.innerHTML === "") {
@@ -24,5 +29,11 @@ function alterna_jogador() {
 function verifica_ganhador() {
     if (casa01.innerHTML != "" && casa01.innerHTML === casa02.innerHTML && casa01.innerHTML === casa03.innerHTML) {
         alert(`Temos uma ganhador: ${casa01.innerHTML}`)
+    }
+    else if (casa04.innerHTML != "" && casa04.innerHTML === casa05 && casa06 === casa04)  {
+        alert(`Temos uma ganhador: ${casa04.innerHTML}`)
+    }
+    else if (casa07.innerHTML != "" && casa07.innerHTML === casa08 && casa09 === casa07){
+        alert(`Temos uma ganhador: ${casa07.innerHTML}`) 
     }
 }
