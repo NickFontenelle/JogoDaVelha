4f267ce12360564137674ff1388d357d3685f0f8
NickFontenelle <thenickfontenelle@gmail.com>
2022-09-09 13:48:43 -0300

    Upgrade1

--- Jogo da velha/index.js
+++ Jogo da velha/index.js
@@ -7,23 +7,22 @@ let casa03 = document.getElementById("casa03");
 
 
 function jogada(casa) {
-    if (casa.innerHTML === ""){
+    if (casa.innerHTML === "") {
         casa.innerHTML = jogador;
-        alterna_jogador();
-
         verifica_ganhador();
+        alterna_jogador();
     }
-        
+
 }
 function alterna_jogador() {
-    if (jogador === "X"){
+    if (jogador === "X") {
         jogador = "O";
-    }else{
+    } else {
         jogador = "X";
     }
 }
 function verifica_ganhador() {
-    if (casa01.innerHTML!="" && casa01.innerHTML===casa02.innerHTML && casa01.innerHTML=== casa03.innerHTML){
+    if (casa01.innerHTML != "" && casa01.innerHTML === casa02.innerHTML && casa01.innerHTML === casa03.innerHTML) {
         alert(`Temos uma ganhador: ${casa01.innerHTML}`)
     }
 }
